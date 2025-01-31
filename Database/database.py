57374e065f75d9a439dd52aaa930a41420162655
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2025-01-31 21:56:31 +0530

    Adding a folder for database

---
+++ Database/database.py
@@ -0,0 +1,24 @@
+import psycopg2 as pg
+import os
+from dotenv import load_dotenv
+# Loading enviroment variables from .env file 
+load_dotenv()
+db_name=os.getenv("name")
+db_user=os.getenv("user")
+db_password=os.getenv("password")
+db_host=os.getenv("host")
+db_port=os.getenv("port")
+# Function to connect with the database 
+def connect_to_database():
+    try:
+        conn=pg.connect(dbname=db_name,user=db_user,password=db_password,host=db_host,port=db_port)
+        print("Connected to database successfully")
+        return conn
+    except Exception as e:
+        print("Connection failed ")
+        return None
+# Test the connection
+if __name__ == "__main__":
+    conn = connect_to_database()
+    if conn:
+        conn.close()  # Close the connection when done
