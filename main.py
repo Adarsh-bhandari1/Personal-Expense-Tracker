5b0fa4d9f16d3c5b014bb0cfe8e7068961633b0e
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2025-02-10 22:04:25 +0530

    adding main_window fuction

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import psycopg2 as pg
 from database import connect_to_database
 import bcrypt 
 from tkinter import *
-root=tk.Tk()
-root.title("Expense_tracker")
-root.geometry('650x500')
-root.mainloop()
+def main_window():
+    root=tk.Tk()
+    root.title("Personal Expense tracker")
+    root.geometry('400x300')
+    login=Label(root,text='Login',font=('Arial',20))
+    login.pack()
+    login_btn=Button(root,text='Login')
+    login_btn.pack()
+    root.mainloop()
+main_window()
