9ac0e0b950a7d2d00d2e7a03c6d5c353c7508b31
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2025-02-12 12:13:13 +0530

    Adding the registration window

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ import psycopg2 as pg
 from database import connect_to_database
 import bcrypt 
 from tkinter import *
+
+# REGISTER WINDOW CODE 
+def register_window():
+    reg_win=tk.Toplevel()
+    reg_win.title('Register')
+    reg_win.geometry('350x250')
+    Label(reg_win , text='username').grid(row=0,column=0,padx=10,pady=5)
+    Entry(reg_win).grid(row=0,column=1,padx=10,pady=5)
+    Label(reg_win,text='password').grid(row=2,column=0,padx=10,pady=5)
+    Entry(reg_win,show='*').grid(row=2,column=1,padx=10)
+    Button(reg_win,text='register').grid(row=3,column=1,pady=3)
+
+
+
+# MAIN WINDOW CODE 
 def main_window():
     root=tk.Tk()
     root.title("Personal Expense tracker")
     root.geometry('400x300')
-    login=Label(root,text='Login',font=('Arial',20))
-    login.pack()
-    login_btn=Button(root,text='Login')
-    login_btn.pack()
+
+    """login_btn=Button(root,text='Login',command=login_window)
+    login_btn.pack()"""
+
+    Register_btn=Button(root, text='Resgister',command=register_window)
+    Register_btn.pack()
     root.mainloop()
 main_window()
+
